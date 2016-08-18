a904ee32d4b70b5c01e5946db10117ac88f3ecad
Bruno <brunogabriel007@hotmail.com>
2016-08-17 21:07:56 -0300

    Iniciando sistema neko

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    </head>
+    <body>
+        <?php
+        echo "<h1>Olásss</h1>";
+        ?>
+    </body>
+</html>
